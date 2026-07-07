06dd3dfa64fb6eac511e2ffc7b625e1b66178d1b
Tushar Kanti Mishra <tushar.mishra1165@gmail.com>
2026-07-07 10:40:25 +0530

    Commit#0021 - Removed cloudfront and custom domain from infra.

--- iac/data.tf
+++ iac/data.tf
@@ -13,10 +13,3 @@ data "aws_iam_policy_document" "content_bucket_policy" {
     resources = ["arn:aws:s3:::${local.infraprefix}-content-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.region}-an/*"]
   }
 }
-
-data "aws_acm_certificate" "custom_domain_ssl_certificate" {
-  domain      = "*.${var.custom_domain_name}"
-  statuses    = ["ISSUED"]
-  types       = ["AMAZON_ISSUED"]
-  most_recent = true
-}
--- iac/variables.tf
+++ iac/variables.tf
@@ -15,9 +15,3 @@ variable "project_owner" {
   description = "Owner of resources in the project"
   default     = "TkM"
 }
-
-variable "custom_domain_name" {
-  type        = string
-  description = "Custom Domain Name for hosting"
-  default     = "tkmworks.co.in"
-}
